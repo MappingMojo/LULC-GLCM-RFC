e43e8e326fff4117fce011ead6184cb33df365e8
MappingMojo <67425655+MappingMojo@users.noreply.github.com>
2021-08-15 00:19:05 +0200

    Update GLCM.R

--- GLCM.R
+++ GLCM.R
@@ -1,19 +1,22 @@
 ##source: https://riptutorial.com/r/example/12880/calculating-glcm-texture
-
+#install required packages
 install.packages('glcm')
 install.packages('sp')
 
+#read libraries
 library(glcm)
 library(sp)
 library(raster)
 
-#inpath <- ("C:/Users/pnkay/Downloads/Thesis/Data/Final4/B8_Nairobi.tif")
-#outpath <- ("C:/Users/pnkay/Downloads/Thesis/Data/Final4/Text21_nir")
+#inpath <- ("path")
+#outpath <- ("path")
 
-r <- raster("C:/Users/pnkay/Downloads/Thesis/Data/Final4/B8_Nairobi.tif")
+#read raster from path (only if a single raster file is in the folder)
+r <- raster("path")
 
 plot(r)
 
+#Generate GLCM textures
 rglcm <- glcm(r, 
               window = c(27,27), 
               shift = c(1,1), 
@@ -26,19 +29,21 @@ rglcm.1 <- stack(rglcm)
 plot(rglcm.1)
 nlayers(rglcm.1)
 
+#name variables
 names(rglcm.1) <- c("mean", "variance", "homogeneity", "contrast", 
                     "dissimilarity", "entropy", "second_moment", "correlation")
 
+#set output directory
+setwd("path")
 
-setwd("C:/Users/pnkay/Downloads/Thesis/Data/Final4/B8/")
-
+#save each variable/band as a seperate file
 for(i in 1:nlayers(rglcm.1)){
   band<-rglcm.1[[i]]
   #save raster in a separate file
   writeRaster(band,paste(names(rglcm.1[[i]]), "B8_27",  '.tif', sep=""))
 }
 
-
+#offload data
 remove(rglcm) 
 
 ################################################################################
